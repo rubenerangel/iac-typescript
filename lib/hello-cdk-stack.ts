985dde2bec2864437cd7b3cfe99ea208b31ef9bc
Rubén Rangel <rubenrang@gmail.com>
2022-06-20 19:08:25 -0500

    First Commit

---
+++ lib/hello-cdk-stack.ts
@@ -0,0 +1,20 @@
+import { Duration, RemovalPolicy, Stack, StackProps } from 'aws-cdk-lib';
+import { Bucket } from 'aws-cdk-lib/aws-s3';
+import { Queue } from 'aws-cdk-lib/aws-sqs';
+import { Construct } from 'constructs';
+
+export class HelloCdkStack extends Stack {
+  constructor(scope: Construct, id: string, props?: StackProps) {
+    super(scope, id, props);
+
+    const queue = new Queue(this, 'HelloCDKQueue', {
+      visibilityTimeout: Duration.seconds(300)
+    });
+
+    const newBucket = new Bucket(this, 'HelloCDKBucket', {
+      versioned: true,
+      removalPolicy: RemovalPolicy.DESTROY,
+      autoDeleteObjects: true
+    });
+  }
+}
